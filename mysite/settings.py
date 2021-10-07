057a5b01ff8c396ff77970022a1fa0820fcf32b1
babitStha <70805868+babitStha@users.noreply.github.com>
2021-10-07 16:35:57 +0545

    Added gunicorn and whitenoise

--- mysite/settings.py
+++ mysite/settings.py
@@ -42,6 +42,9 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -121,6 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT ='staticfiles'
 STATICFILES_DIRS =[
     os.path.join(BASE_DIR, "static")
 ]
